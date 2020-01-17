21f9471864f116b2877810795cb016fd9e6a3b52
Nicolas Burniske <nicoburniske@gmail.com>
2020-01-16 22:02:25 -0500

    added playGame method signature

--- src/main/java/ChessBoard.java
+++ src/main/java/ChessBoard.java
@@ -1,7 +1,10 @@
 public class ChessBoard {
-    IPiece[][] board = new IPiece[8][8];
+   IPiece[][] board = new IPiece[8][8];
 
     public ChessBoard() {
+
+    }
+    boolean playGame(int fromX, int fromY, int toX, int toY) {
 
     }
 }
