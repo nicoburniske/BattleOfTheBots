d06a2807b731771993a8ed2d65383a3bf862d9d9
Yoshar <YaroshPWebsites@icloud.com>
2020-01-17 22:45:15 -0500

    added two knight tests

--- src/test/java/TestChessBoard.java
+++ src/test/java/TestChessBoard.java
@@ -17,6 +17,121 @@ public class TestChessBoard {
     }
 
     @Test
+    public void testMoveKnights(){
+        //  [WHITE] - move queen-side knight
+        assertEquals(true, board1.playGame(1, 0, 2, 2));
+
+        //  [BLACK] - move queen-side knight
+        assertEquals(true, board1.playGame(1, 7, 2, 5));
+
+        //  [WHITE] - move previous knight
+        assertEquals(true, board1.playGame(2, 2, 4, 3));
+
+        //  [BLACK] - move king-side knight
+        assertEquals(true, board1.playGame(6, 7, 5, 5));
+
+        //  [WHITE] - move king-side knight
+        assertEquals(true, board1.playGame(6, 0, 5, 2));
+
+        //  [BLACK] - move previous knight
+        assertEquals(true, board1.playGame(5, 5, 3, 4));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(5, 2, 4, 4));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(2, 5, 3, 3));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(4, 4, 2, 5));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(3, 3, 5, 2));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(4, 3, 5, 5));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(3, 4, 2, 2));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(2, 5, 1, 7));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(5, 2, 6, 0));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(5, 5, 6, 7));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(2, 2, 1, 0));
+
+    }
+
+    @Test
+    public void testMoveKnights_Capture(){
+        //  [WHITE] - move queen-side knight
+        assertEquals(true, board1.playGame(1, 0, 2, 2));
+
+        //  [BLACK] - move queen-side knight
+        assertEquals(true, board1.playGame(1, 7, 2, 5));
+
+        //  [WHITE] - move previous knight
+        assertEquals(true, board1.playGame(2, 2, 4, 3));
+
+        //  [BLACK] - move king-side knight
+        assertEquals(true, board1.playGame(6, 7, 5, 5));
+
+        //  [WHITE] - move king-side knight
+        assertEquals(true, board1.playGame(6, 0, 5, 2));
+
+        //  [BLACK] - move previous knight
+        assertEquals(true, board1.playGame(5, 5, 3, 4));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(5, 2, 4, 4));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(2, 5, 3, 3));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(4, 4, 2, 5));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(3, 4, 2, 2));
+
+        //  [WHITE] - move knight, capture black knight
+        assertEquals(true, board1.playGame(4, 3, 2, 2));
+
+        //  [BLACK] - move knight, capture white knight
+        assertEquals(true, board1.playGame(3, 3, 2, 5));
+
+        //  [WHITE] - move knight
+        assertEquals(true, board1.playGame(2, 2, 3, 4));
+
+        //  [BLACK] - move knight
+        assertEquals(true, board1.playGame(2, 5, 3, 3));
+
+        //  [WHITE] - move knight, capture pawn
+        assertEquals(true, board1.playGame(3, 4, 4, 6));
+
+        //  [BLACK] - move knight, capture pawn
+        assertEquals(true, board1.playGame(3, 3, 4, 1));
+
+        //  [WHITE] - move knight, capture bishop
+        assertEquals(true, board1.playGame(4, 6, 2, 7));
+
+        //  [BLACK] - move knight, capture bishop
+        assertEquals(true, board1.playGame(4, 1, 2, 0));
+
+        //  [WHITE] - move knight, capture pawn
+        assertEquals(true, board1.playGame(2, 7, 0, 6));
+
+        //  [BLACK] - move knight, capture bishop
+        assertEquals(true, board1.playGame(2, 0, 0, 1));
+    }
+
+    @Test
     public void testMovePawns() {
         // move white King pawn
         assertEquals(true, board1.playGame(4, 1, 4, 3));
