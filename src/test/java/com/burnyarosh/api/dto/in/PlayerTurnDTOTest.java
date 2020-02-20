37655bc54add1862a16be170e9b3f5b0bc9014ac
Nick Burniske <nicoburniske@gmail.com>
2020-02-20 01:50:47 -0500

    added dto tests for mapping

--- src/test/java/com/burnyarosh/api/dto/in/PlayerTurnDTOTest.java
+++ src/test/java/com/burnyarosh/api/dto/in/PlayerTurnDTOTest.java
@@ -1,5 +1,35 @@
+package com.burnyarosh.api.dto.in;
+
+import com.burnyarosh.api.dto.common.CoordDTO;
+import com.burnyarosh.api.processor.utils.Mapper;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTurnDTOTest {
+    JsonObject playerTurnJson;
+    PlayerTurnDTO turn1;
+    PlayerTurnDTO turn2;
+
+    @Test
+    public void testJSONMapping() {
+        JsonObject origin = new JsonObject().put("x", 0).put("y", 50);
+        JsonObject target = new JsonObject().put("x", 333).put("y", 50);
+
+        CoordDTO orig = Mapper.getJsonAsClass(origin, CoordDTO.class);
+        CoordDTO targ = Mapper.getJsonAsClass(target, CoordDTO.class);
+
+        playerTurnJson = new JsonObject().put("origin", origin).put("target", target);
+
+        turn1 = new PlayerTurnDTO();
+        turn1.setOrigin(orig);
+        turn1.setTarget(targ);
+
+        turn2 = Mapper.getJsonAsClass(playerTurnJson, PlayerTurnDTO.class);
+        System.out.println(turn2.toJsonString());
+        System.out.println(turn1.toJsonString());
+    }
 
 }
--- src/test/java/com/burnyarosh/dto/in/AbstractRequestDTOTest.java
+++ src/test/java/com/burnyarosh/dto/in/AbstractRequestDTOTest.java
@@ -1,30 +1,49 @@
 package com.burnyarosh.dto.in;
 
+import com.burnyarosh.api.dto.in.JoinLobbyDTO;
 import com.burnyarosh.api.dto.in.NewPlayerDTO;
+import com.burnyarosh.api.exception.JsonMappingException;
+import com.burnyarosh.api.processor.utils.Mapper;
 import io.vertx.core.json.JsonObject;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class AbstractRequestDTOTest {
 
     JsonObject validNewPlayerJson, invalidNewPlayerJson;
     NewPlayerDTO validNewPlayerObject;
+    JsonObject joinNewLobbyInvalidJson;
 
     @Before
     public void init() {
         this.validNewPlayerJson =  new JsonObject();
-        //this.validNewPlayerJson.put("type", "new_player");
         this.validNewPlayerJson.put("username", "a_username");
         this.validNewPlayerObject = new NewPlayerDTO();
         this.validNewPlayerObject.setUsername("a_username");
+
+        this.invalidNewPlayerJson = new JsonObject();
+        this.invalidNewPlayerJson.put("poop", "crap");
+        this.joinNewLobbyInvalidJson = new JsonObject();
+        this.joinNewLobbyInvalidJson.put("playerGUID", "aguid");
     }
 
     @Test
     public void isValidRequest() {
         // JsonObject test = new JsonObject(Json.encode(new NewPlayerDTO("ausername")));
-        NewPlayerDTO dto = this.validNewPlayerJson.mapTo(com.burnyarosh.api.dto.in.NewPlayerDTO.class);
+        NewPlayerDTO dto = Mapper.getJsonAsClass(validNewPlayerJson, NewPlayerDTO.class);
         assertTrue(dto.equals(validNewPlayerObject));
+    }
+    @Test (expected = JsonMappingException.class)
+    public void invalidRequest() {
+        NewPlayerDTO dto = Mapper.getJsonAsClass(invalidNewPlayerJson, NewPlayerDTO.class);
+    }
+    @Test
+    public void invalidRequest2() {
+        JoinLobbyDTO dto = Mapper.getJsonAsClass(joinNewLobbyInvalidJson, JoinLobbyDTO.class);
+        assertTrue(dto.getGameGUID() == null);
+        assertFalse(dto.isValidRequest());
     }
 }
