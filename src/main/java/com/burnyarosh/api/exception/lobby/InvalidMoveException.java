a6bf80e7493ef49f342756d5ad954aa051fab72d
Nick Burniske <nicoburniske@gmail.com>
2020-02-20 01:50:12 -0500

    added new exception for invalid move

--- src/main/java/com/burnyarosh/api/exception/lobby/InvalidMoveException.java
+++ src/main/java/com/burnyarosh/api/exception/lobby/InvalidMoveException.java
@@ -1,4 +1,17 @@
 package com.burnyarosh.api.exception.lobby;
 
-public class InvalidMoveException {
+import com.burnyarosh.api.processor.MessageFailureHandler;
+import io.vertx.core.eventbus.Message;
+
+public class InvalidMoveException extends RuntimeException implements HandledLobbyException {
+    Message message;
+
+    public InvalidMoveException(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void callLobbyFailureHandler(MessageFailureHandler handler) {
+        handler.handleInvalidMove(message);
+    }
 }
