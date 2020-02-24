3a5b6c9e49413a8ee7da976f9a4fd1b67324220a
Nick Burniske <nicoburniske@gmail.com>
2020-02-23 23:28:39 -0500

    added additional DTO for playerupdate and added codec

--- src/main/java/com/burnyarosh/api/dto/internal/PlayerUpdateDTO.java
+++ src/main/java/com/burnyarosh/api/dto/internal/PlayerUpdateDTO.java
@@ -1,4 +1,25 @@
 package com.burnyarosh.api.dto.internal;
 
-public class PlayerUpdate {
+import com.burnyarosh.api.dto.IDTO;
+import io.vertx.core.json.JsonObject;
+
+public class PlayerUpdateDTO implements IDTO {
+    private String PlayerGUID;
+    private JsonObject json;
+
+    public String getPlayerGUID() {
+        return PlayerGUID;
+    }
+
+    public void setPlayerGUID(String playerGUID) {
+        PlayerGUID = playerGUID;
+    }
+
+    public JsonObject getJson() {
+        return json;
+    }
+
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
 }
