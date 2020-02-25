87c3dccec9137dbb0d277ea1c25b5065acf36c39
Nick Burniske <nicoburniske@gmail.com>
2020-02-25 16:03:27 -0500

    restructured event bus addresses

--- src/main/java/com/burnyarosh/api/processor/utils/EventBusAddress.java
+++ src/main/java/com/burnyarosh/api/processor/utils/EventBusAddress.java
@@ -1,14 +1,16 @@
 package com.burnyarosh.api.processor.utils;
 
 public enum EventBusAddress {
-    NEW_PLAYER_ADDRESS("new.player"),
-    NEW_MOVE_ADDRESS("burnyarosh.lobby.move."),
-    JOIN_LOBBY_ADDRESS("join.lobby"),
-    JOIN_ANY_LOBBY("join.any.lobby"),
-    NEW_LOBBY_ADDRESS("new.lobby"),
-    LIST_LOBBY_ADDRESS("list.lobby"),
-    LIST_PLAYER_ADDRESS("list.player"),
-    LOBBY_BASE_ADDRESS("burnyarosh.lobby."),
+    NEW_PLAYER_ADDRESS("burnyarosh.lobby.new.player"),
+    NEW_MOVE_ADDRESS("burnyarosh.lobby.move"),
+    JOIN_LOBBY_ADDRESS("burnyarosh.lobby.join"),
+    JOIN_ANY_LOBBY("burnyarosh.lobby.join.any"),
+    REMOVE_PLAYER_ADDRESS("burnyarosh.lobby.remove.player"),
+    NEW_LOBBY_ADDRESS("burnyarosh.lobby.new.lobby"),
+    LIST_LOBBY_ADDRESS("burnyarosh.lobby.list.lobby"),
+    LIST_PLAYER_ADDRESS("burnyarosh.lobby.list.player"),
+    GAME_BASE_ADDRESS("burnyarosh.game.%s"),
+    GAME_MOVE_ADDRESS("burnyarosh.game.%s.move"),
     UPDATE_PLAYERS_ADDRESS("burnyarosh.player.connection.update");
 
     private String address;
