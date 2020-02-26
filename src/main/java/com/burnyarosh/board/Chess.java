f3c52068df3c4b14977f472f17655103eb6c58d7
Yoshar <peteryarosh+github@gmail.com>
2020-02-26 02:25:29 -0500

    finished new Move pipeline

--- src/main/java/com/burnyarosh/board/Chess.java
+++ src/main/java/com/burnyarosh/board/Chess.java
@@ -51,9 +51,8 @@ public class Chess {
         } else {
             Coord origin = new Coord(originX, originY);
             Coord target = new Coord(targetX, targetY);
-            if (this.isValidMove(origin, target)){
-                //  TODO: CLASSIFY MOVE type
-                //this.b.executeMove();
+            if (isValidMove(this.b, this.turn, origin, target)){
+                this.b.executeMove(origin, target, promotion);
                 this.nextTurn();
                 //this.updatePreviousMove();
                 return true;
@@ -114,7 +113,7 @@ public class Chess {
         if (isInCheck(this.b, this.turn)){
             for (IPiece p : this.b.getPieces(this.turn)){
                 for (Coord c : p.getPossibleMoves(this.b.getBoardArrayCopy(), this.b.getMoveHistory())){
-                    if (this.tryMove(p.getCoord(), c)){
+                    if (tryMove(this.b, this.turn, p.getCoord(), c)){
                         return false;
                     }
                 }
@@ -124,18 +123,18 @@ public class Chess {
         return false;
     }
 
-    private boolean tryMove(Coord origin, Coord target){
-        Board temp = this.b.copy();
+    private static boolean tryMove(Board b, Color turn, Coord origin, Coord target){
+        Board temp = b.copy();
         temp.executeMove(origin, target, 'Q');  //  TODO: SHOULD I ADD METHOD PARAM FOR PROMOTION KEY
-        return !isInCheck(temp, this.turn);
+        return !isInCheck(temp, turn);
     }
 
     //  TODO: PENDING REVIEW - (HAS CURRENT LOGIC, COULD BE IMPROVED)
-    private boolean isValidEnPassant(Coord origin, Coord target){
-        if (this.b.getPieceAtCoord(origin) instanceof Pawn && Math.abs(origin.getY() - target.getY()) == 1 && Math.abs(origin.getX() - target.getX()) == 1 && this.b.getPieceAtCoord(target) == null){
-            if (origin.getY() == (this.b.getPieceAtCoord(origin).getIsBlack() ? 3 : 4)){
-                if (this.b.getMoveHistory().size() > 3){        //  TODO: VERRIFY THIS
-                    return (this.b.getMoveHistory().get(this.b.getMoveHistory().size() - 2).getPiece() instanceof Pawn) && (this.b.getPieceAtCoord(new Coord(target.getX(), origin.getY())).getMoveCount() == 1);
+    private static boolean isValidEnPassant(Board b, Coord origin, Coord target){
+        if (b.getPieceAtCoord(origin) instanceof Pawn && Math.abs(origin.getY() - target.getY()) == 1 && Math.abs(origin.getX() - target.getX()) == 1 && b.getPieceAtCoord(target) == null){
+            if (origin.getY() == (b.getPieceAtCoord(origin).getIsBlack() ? 3 : 4)){
+                if (b.getMoveHistory().size() > 3){        //  TODO: VERRIFY THIS
+                    return (b.getMoveHistory().get(b.getMoveHistory().size() - 2).getPiece() instanceof Pawn) && (b.getPieceAtCoord(new Coord(target.getX(), origin.getY())).getMoveCount() == 1);
                 }
             }
         }
@@ -151,7 +150,7 @@ public class Chess {
         return true;
     }
 
-    protected static boolean isInDangerBetween(Board b, Color turn, Coord start, Coord end){
+    public static boolean isInDangerBetween(Board b, Color turn, Coord start, Coord end){
         for ( Coord c : start.calculatePointsBetweenInclusiveEnd((end))) {
             if (isInDanger(b, turn, c)) return true;
         }
@@ -169,9 +168,9 @@ public class Chess {
         return b.getPieceAtCoord(origin).isValidMove(b.getBoardArrayCopy(), origin, target);
     }
 
-    private boolean isValidMoveBoolean(Coord origin, Coord target){
+    public static boolean isValidMoveBoolean(Board b, Color turn, Coord origin, Coord target){
         try {
-            return this.isValidMove(origin, target);
+            return isValidMove(b, turn, origin, target);
         } catch (Exception e) {
             return false;
         }
@@ -181,23 +180,23 @@ public class Chess {
     //TODO: if isInCheck and the resulting move does not remove the player from being in check, then it is an invalid move
     // use constructor that takes in a 2d board to make a new chessBoard (feed it this.getBoard(), and this.isWhiteTurn()) to have a copy of the board.
     // perform the move on this board and if the king is still in check (for the player that moved, than the move is invalid.
-    private boolean isValidMove(Coord origin, Coord target){
+    private static boolean isValidMove(Board b, Color turn, Coord origin, Coord target){
         if (!origin.isInsideBoard() || !target.isInsideBoard()) {
             throw new IllegalArgumentException("Coordinate outside of board");
         } else {
-            IPiece from = this.b.getPieceAtCoord(origin);
-            IPiece to = this.b.getPieceAtCoord(target);
+            IPiece from = b.getPieceAtCoord(origin);
+            IPiece to = b.getPieceAtCoord(target);
             if (from == null) {
                 throw new IllegalArgumentException("Must move a piece");
             } else if (origin.equals(target)) {
                 throw new IllegalArgumentException("Cannot move to same space");
-            } else if (from.getIsBlack() && this.turn == Color.WHITE || !from.getIsBlack() && this.turn == Color.BLACK) {
+            } else if (from.getIsBlack() && turn == Color.WHITE || !from.getIsBlack() && turn == Color.BLACK) {
                 throw new IllegalArgumentException("Other player's move");
             } else if (to != null && (from.getIsBlack() && to.getIsBlack() || !(from.getIsBlack() || to.getIsBlack()))) {
                 throw new IllegalArgumentException("Cannot move to square occupied by piece of same color");
-            } else if (!from.isValidMove(this.b.getBoardArrayCopy(), origin, target) && !this.isValidEnPassant(origin, target)) {
+            } else if (!from.isValidMove(b.getBoardArrayCopy(), origin, target) && !isValidEnPassant(b, origin, target)) {
                 throw new IllegalArgumentException("Invalid move");
-            } else if (isInCheck(this.b, this.turn) && !this.tryMove(origin, target)) {
+            } else if (isInCheck(b, turn) && !tryMove(b, turn, origin, target)) {
                 throw new IllegalStateException("Move results with King in check");
             }
             return true;
